c7fd02cbf350b09724c5dca4b0fe7f085e95471f
pp-mo <patrick.peglar@metoffice.gov.uk>
2013-01-06 09:59:55 +0000

    remove all tabs

--- merge_egs_1.py
+++ merge_egs_1.py
@@ -5,88 +5,88 @@ Created on Jan 4, 2013
 '''
 do_real = False
 if do_real:
-	import iris
-	import iris.tests.stock
+    import iris
+    import iris.tests.stock
 
-	def tm(cubes):    
-	  return iris.cube.CubeList(cubes).merge()
+    def tm(cubes):    
+      return iris.cube.CubeList(cubes).merge()
 
-	t2d = iris.tests.stock.simple_2d(with_bounds=False)
-	print 't2d'
-	print t2d
+    t2d = iris.tests.stock.simple_2d(with_bounds=False)
+    print 't2d'
+    print t2d
 
-	nz = 5
-	t3d = [t2d.copy() for iz in range(nz)]
-	for iz in range(nz):
-	  t3d[iz].add_aux_coord(iris.coords.DimCoord([iz],long_name='baz'))
-	t3d = iris.cube.CubeList(t3d).merge()[0]
-	print 't3d'
-	print t3d
+    nz = 5
+    t3d = [t2d.copy() for iz in range(nz)]
+    for iz in range(nz):
+      t3d[iz].add_aux_coord(iris.coords.DimCoord([iz],long_name='baz'))
+    t3d = iris.cube.CubeList(t3d).merge()[0]
+    print 't3d'
+    print t3d
 
 # order we wish to treat the names in (which is *not* in dimension order)
 coord_names = ['baz','foo','bar']
 
 if not do_real:
-	import numpy as np
-	
-	class DummyCoord(object):
-		def __init__(self, points):
-			self.points = points
-			
-	class DummyCube(object):
-		def __init__(self, array=None, dims_list=None):
-			if array is not None:
-				if not isinstance(array, np.ndarray):
-					dims_list = array
-					array = None
-				else:
-					n_dims = len(array.shape)
-					names = coord_names[-n_dims:]
-					dims_list = zip(names, array.shape)
-					#print 'cube from array : shape=', array.shape
-					#print '  dimslist = ', dims_list
-			self.shape = tuple([n_points for (name, n_points) in dims_list])
-			self._coord_dims = {}
-			self._coord_names = []
-			self._coords = []
-			for (name, n_points) in dims_list:
-				self._coord_dims[name] = len(self._coord_names)
-				self._coord_names.append(name)
-				self._coords.append(DummyCoord(range(n_points)))
-			if array is None:
-				array = np.zeros(self.shape)
-				array.flat = np.arange(array.size)
-			self.data = array
-			
-		def coord(self, name):
-			return self._coords[self._coord_dims[name]]
+    import numpy as np
+    
+    class DummyCoord(object):
+        def __init__(self, points):
+            self.points = points
+            
+    class DummyCube(object):
+        def __init__(self, array=None, dims_list=None):
+            if array is not None:
+                if not isinstance(array, np.ndarray):
+                    dims_list = array
+                    array = None
+                else:
+                    n_dims = len(array.shape)
+                    names = coord_names[-n_dims:]
+                    dims_list = zip(names, array.shape)
+                    #print 'cube from array : shape=', array.shape
+                    #print '  dimslist = ', dims_list
+            self.shape = tuple([n_points for (name, n_points) in dims_list])
+            self._coord_dims = {}
+            self._coord_names = []
+            self._coords = []
+            for (name, n_points) in dims_list:
+                self._coord_dims[name] = len(self._coord_names)
+                self._coord_names.append(name)
+                self._coords.append(DummyCoord(range(n_points)))
+            if array is None:
+                array = np.zeros(self.shape)
+                array.flat = np.arange(array.size)
+            self.data = array
+            
+        def coord(self, name):
+            return self._coords[self._coord_dims[name]]
 
-		def coords(self, name):
-			try_index = self._coord_dims.get(name)
-			if try_index is None:
-				return []
-			return [self._coords[try_index]]
-		
-		def __getitem__(self, indexes):
-			#print 'getitem : ',indexes
-			array = self.data[indexes]
-			#print '  array = ', self.data
-			#print '  subarray = ', array
-			return DummyCube(array)
-		
-		def __str__(self):
-			return '<DummyCube: '+str(self.data)+'>'
-			
-	nz = 5
-	coord_dims = [3,4,nz]
-	coord_dims = dict(zip(coord_names, coord_dims))
-#	print 'coord dims = ', coord_dims
-	# note for these, coords must occur IN DIMENSION ORDER
-	t2d = DummyCube([(name, coord_dims[name]) for name in ('bar', 'foo')])
-	t3d = DummyCube([(name, coord_dims[name]) for name in ('baz', 'bar', 'foo')])
-#	print 'dummy t2d = ', t2d
-#	print 'dummy t2d[2:,2:] = ',t2d[2:,2:]
-#	print 'dummy t3d = ', t3d
+        def coords(self, name):
+            try_index = self._coord_dims.get(name)
+            if try_index is None:
+                return []
+            return [self._coords[try_index]]
+        
+        def __getitem__(self, indexes):
+            #print 'getitem : ',indexes
+            array = self.data[indexes]
+            #print '  array = ', self.data
+            #print '  subarray = ', array
+            return DummyCube(array)
+        
+        def __str__(self):
+            return '<DummyCube: '+str(self.data)+'>'
+            
+    nz = 5
+    coord_dims = [3,4,nz]
+    coord_dims = dict(zip(coord_names, coord_dims))
+#    print 'coord dims = ', coord_dims
+    # note for these, coords must occur IN DIMENSION ORDER
+    t2d = DummyCube([(name, coord_dims[name]) for name in ('bar', 'foo')])
+    t3d = DummyCube([(name, coord_dims[name]) for name in ('baz', 'bar', 'foo')])
+#    print 'dummy t2d = ', t2d
+#    print 'dummy t2d[2:,2:] = ',t2d[2:,2:]
+#    print 'dummy t3d = ', t3d
 
 
 
@@ -124,28 +124,23 @@ def cube_from_notation_string(cube_string):
         coord_indices[coord_name].append(point_index)
     #print 'coord indices:', coord_indices
     if len(coord_indices['baz']) == 0:
-        #print '2d slice: ',coord_indices['bar'], coord_indices['foo']
-        #result_cube = t2d[coord_indices['bar'], coord_indices['foo']]
-        result_cube = t2d[coord_indices['bar'], :]
-        result_cube = result_cube[:, coord_indices['foo']]
+        print '2d slice: ',coord_indices['bar'], coord_indices['foo']
+        result_cube = t2d[coord_indices['bar'], coord_indices['foo']]
     else:
         print '3d slice: ',coord_indices['bar'], coord_indices['foo']
         result_cube = t3d[coord_indices['baz'], coord_indices['bar'], coord_indices['foo']]
-        result_cube = t2d[coord_indices['baz'], :, :]
-        result_cube = result_cube[:, coord_indices['bar'], :]
-        result_cube = result_cube[:, :, coord_indices['foo']]
     return result_cube
 
 def test_cube_2_notation_string():
-	tst_specs = [
-		('t2d',t2d),
-		('t3d',t3d),
-		('t3d[0,1:2,2:]',t3d[0,1:2,2:])
-	]
-	for (tst_str, tst_cube) in tst_specs:
-		print 'test cube = ', tst_str
-		print tst_cube
-		print '  --> ',cube_2_notation_string(tst_cube)
+    tst_specs = [
+        ('t2d',t2d),
+        ('t3d',t3d),
+        ('t3d[0,1:2,2:]',t3d[0,1:2,2:])
+    ]
+    for (tst_str, tst_cube) in tst_specs:
+        print 'test cube = ', tst_str
+        print tst_cube
+        print '  --> ',cube_2_notation_string(tst_cube)
 
 print
 print 'TEST cube_2_notation_string...'
@@ -153,15 +148,15 @@ test_cube_2_notation_string()
 print
 
 def test_cube_from_notation_string():
-	tst_specs = [
-		'ab12',
-		'Pac21',
-		'PQa12',
-	]
-	for tst_str in tst_specs:
-		print tst_str
-		t = cube_from_notation_string(tst_str)
-		print t
+    tst_specs = [
+        'ab12',
+        'Pac21',
+        'PQa12',
+    ]
+    for tst_str in tst_specs:
+        print tst_str
+        t = cube_from_notation_string(tst_str)
+        print t
 
 print
 print 'TEST cube_FROM_notation_string...'
